b7fe9fe322a99fc3b8125a260c1f6f096baad031
Alan <alandsn137@gmail.com>
2025-04-11 15:35:16 -0300

    optimized code by reducing bolierplate

--- src/main/java/com/decssoft/languagemanager/LanguageManager.java
+++ src/main/java/com/decssoft/languagemanager/LanguageManager.java
@@ -1,20 +1,15 @@
 package com.decssoft.languagemanager;
 
-import com.decssoft.languagemanager.utils.Language;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class LanguageManager extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        var fxml = new FXMLLoader(getClass().getResource("/com/decssoft/languagemanager/views/Main.fxml"),
-                Language.getBundle());
-        var scene = new Scene(fxml.load());
-        stage.setScene(scene);
-        stage.show();
+        log.info("loaded");
     }
 
     public static void main(String args[]) {
