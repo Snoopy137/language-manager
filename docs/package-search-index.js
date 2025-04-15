761bbd50f8b79a204ced604e485aedbedb79ab9f
Alan <alandsn137@gmail.com>
2025-04-15 18:23:56 -0300

    Update Javadocs for new features introduced in version 1.1.0

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.github.snoopy137.languagemanager"},{"l":"io.github.snoopy137.languagemanager.utils"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.github.snoopy137.languagemanager"},{"l":"io.github.snoopy137.languagemanager.annotations"},{"l":"io.github.snoopy137.languagemanager.utils"}];updateSearchResults();
